50bdd566fbf185d96af6aa52af4a7bc0e9e00765
Vinayak Suryavanshi <95699405+vbs30@users.noreply.github.com>
2024-12-02 15:37:34 +0530

    Update settings.py

--- vbsprojone/settings.py
+++ vbsprojone/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'core',
     'item',
     'dashboard',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
