3d3f4bfae5b6a422d8345f77382d7b119d241bc2
Vinayak Suryavanshi <95699405+vbs30@users.noreply.github.com>
2024-12-02 16:45:19 +0530

    Update settings.py

--- vbsprojone/settings.py
+++ vbsprojone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,8 +128,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # For your 'static' folder in the root
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # A single folder to collect all static files
+
+
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
